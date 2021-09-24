40ab526caec2219e5f3081285c136499b250dee0
avenugopal-at-178736008566 <annamalai.venugopal@exterro.com>
2021-09-24 08:59:20 +0530

    Committed for changes to feedback form

--- resort-booking/src/app/body/gallery/gallery.component.ts
+++ resort-booking/src/app/body/gallery/gallery.component.ts
@@ -11,12 +11,13 @@ export class GalleryComponent implements OnInit {
     url:""
   };
   public counter = 1;
+  public timer:any;
   constructor() {  
       this.image.url = "assets/images/gallery/"+1+".jpeg";
   }
 
   ngOnInit(): void {
-    setInterval(() => {
+    this.timer = setInterval(() => {
       console.log("Changing the image")
       this.counter++;
       if(this.counter > 20){
@@ -24,6 +25,37 @@ export class GalleryComponent implements OnInit {
       }
       this.image.url = "assets/images/gallery/"+this.counter+".jpeg";
     }, 5000);
+  }
+
+  resetTimer(count:any){
+    this.counter = count;
+    clearInterval(this.timer);
+    this.timer = setInterval(
+      () => {
+      console.log("Changing the image")
+      this.counter++;
+      if(this.counter > 20){
+        this.counter = 1;
+      }
+      this.image.url = "assets/images/gallery/"+this.counter+".jpeg";}, 5000);
+  }
+
+  prevImage(){
+    this.counter--;
+    if(this.counter < 1){
+      this.counter = 20;
+    }
+    this.image.url = "assets/images/gallery/"+this.counter+".jpeg";
+    this.resetTimer(this.counter-1); 
+  }
+
+  nextImage(){
+    this.counter++;
+    if(this.counter > 20){
+      this.counter = 1;
+    }
+    this.image.url = "assets/images/gallery/"+this.counter+".jpeg";
+    this.resetTimer(this.counter-1);
   }
 
 }
