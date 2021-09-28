e540f7d7c406366b67d80ed5dce79c9c79e9f8f2
avenugopal-at-178736008566 <annamalai.venugopal@exterro.com>
2021-09-28 07:59:34 +0530

    Committed for booking form

--- resort-booking/src/app/body/booking/booking.component.ts
+++ resort-booking/src/app/body/booking/booking.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  public select: string = "";
+
+  constructor() { 
+  }
 
   ngOnInit(): void {
+ 
+  }
+
+  changeCottageType(event: any){
+     let noOfPeople = event.target.value;
+    if( noOfPeople > 2 && noOfPeople < 13){
+      this.select = "1";
+    } else if(noOfPeople > 12 && noOfPeople < 26){
+      this.select = "2";
+    }
   }
 
 }
